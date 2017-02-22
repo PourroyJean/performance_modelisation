6c54aa7a483fb55d4ce6ff9da19109175900877b
Jean <jean.pourroy@gmail.com>
2017-02-22 11:36:30 +0100

    Gestion des dependances

--- src/tool_freq/tool_freq_generators.cpp
+++ src/tool_freq/tool_freq_generators.cpp
@@ -1,8 +1,9 @@
 //
 // Created by Jean Pourroy on 20/02/2017.
 //
-
+#include <stdio.h>
 #include <iostream>
+#include <sstream>
 #include "tool_freq_generators.h"
 #include "tool_freq_misc.h"
 
@@ -14,17 +15,23 @@
 
 
 void Tool_freq_generators::generate_assembly() {
+    stringstream tmpSS;
+    string strNbIteration;
+    tmpSS << BENCH_NB_ITERATION;
+    tmpSS >> strNbIteration;
+
     WC("__asm__ (\"myBench: \" ");
-    for (auto instruction: * mInstructions_set){
-        const char * s = instruction.c_str();
-        fprintf(P_FPC, "\"");
+    for (auto instruction: *mInstructions_set) {
+        const char *s = instruction.c_str();
+        fprintf(P_FPC, "\t\t\"");
         fprintf(P_FPC, instruction.c_str());
         fprintf(P_FPC, "\"\n");
 
     }
 //    WC("\"       sub    $0x1, %%eax;\"");
     fprintf(P_FPC, "\"  sub    $0x1, %%%%eax;\"\n");
-    WC("\"  jnz    myBench\" : : \"a\" (10000)");
+    string jumpLine = "\"  jnz    myBench\" : : \"a\" (" + strNbIteration + ")";
+    fprintf(P_FPC, jumpLine.c_str());
     WC(");");
 }
 
@@ -57,15 +64,18 @@ void Tool_freq_generators::generate_header() {
 }
 
 
-int Tool_freq_generators::Get_register_source(){
-    if (mParameters->P_DEPENDENCY){
+int Tool_freq_generators::Get_register_source() {
+    if (mParameters->P_DEPENDENCY) {
         return mPrevious_target_register;
-    }else{
+    } else {
         return 1;
     }
 }
 
-int Tool_freq_generators::Get_register_cible(){
+int Tool_freq_generators::Get_register_cible() {
+    if (mPrevious_target_register == 15) {
+        mPrevious_target_register = 1;
+    }
     mPrevious_target_register++;
     return mPrevious_target_register;
 }
@@ -75,45 +85,39 @@ void Tool_freq_generators::generate_instructions() {
     DEBUG_PRINT("-- Generating instructions vector\n");
 
     mInstructions_set->clear();
-    mPrevious_target_register  = 1;
+    mPrevious_target_register = 1;
 
     for (auto operation : *mOperations_set) {
         //v add p d
         string instruction = mPrefix + operation + mSuffix + mPrecision + " ";
-
-        instruction += "%%%%"  + mRegister_name + "0, ";
-        instruction += "%%%%"  + mRegister_name + to_string (Get_register_source()) + ", ";
-        instruction += "%%%%"  + mRegister_name + to_string (Get_register_cible())  + "; ";
-
-
-
+        instruction += "%%%%" + mRegister_name + "0, ";
+        instruction += "%%%%" + mRegister_name + to_string(Get_register_source()) + ", ";
+        instruction += "%%%%" + mRegister_name + to_string(Get_register_cible()) + "; ";
         mInstructions_set->push_back(instruction);
     }
 
 }
 
 
-
 void Tool_freq_generators::Init_Generator() {
     DEBUG_PRINT("-- Init Generator register, prefix, suffix, and precision \n");
-    if (mParameters->P_WIDTH == 64 || mParameters->P_WIDTH == 128){
+    if (mParameters->P_WIDTH == 64 || mParameters->P_WIDTH == 128) {
         mRegister_name = "xmm";
     }
-    if (mParameters->P_WIDTH == 256){
+    if (mParameters->P_WIDTH == 256) {
         mRegister_name = "ymm";
     }
-    if (mParameters->P_WIDTH == 512){
+    if (mParameters->P_WIDTH == 512) {
         mRegister_name = "zmm";
     }
     //only v instructions supported
     mPrefix = "v";
-    if ( mParameters->P_WIDTH == 64){
+    if (mParameters->P_WIDTH == 64) {
         mSuffix = "s";
-    }
-    else {
+    } else {
         mSuffix = "p";
     }
-    if (!mParameters->P_PRECISION.compare("single")){
+    if (!mParameters->P_PRECISION.compare("single")) {
         mPrecision = "s";
     } else {
         mPrecision = "d";
@@ -164,7 +168,7 @@ Tool_freq_generators::Tool_freq_generators(Tool_freq_parameters *param) {
 
     mParameters = param;
     mRegister_name = "xmm";
-    mPrevious_target_register= 1;
+    mPrevious_target_register = 1;
     mPrefix = "v";
     mOperations_set = new vector<string>();
     mSuffix = "s";
