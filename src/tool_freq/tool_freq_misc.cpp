7d6f2c5aaf6e24bdf17d29261bdf73bb3b365a2f
Jean <jean.pourroy@gmail.com>
2017-02-24 19:13:18 +0100

    Calcul la frequence

--- src/tool_freq/tool_freq_misc.cpp
+++ src/tool_freq/tool_freq_misc.cpp
@@ -6,9 +6,29 @@
 #include "tool_freq_misc.h"
 
 
-
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 void usage() {
     fprintf(stderr,
             "Usage: ./tool_freq [-I INSTRUCTIONS ] [-W WIDTH] [-O operations] [-B BINDING] [-vh]\n");
+}
+
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
 }
