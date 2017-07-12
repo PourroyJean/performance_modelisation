a66dd751401e050890cb32ba707c867c50d1aa64
Jean <jean.pourroy@gmail.com>
2017-07-12 14:55:29 +0200

    restructuration et debut script hw

--- src/tool_hw_description/hw_update.sh
+++ src/tool_hw_description/hw_update.sh
@@ -15,29 +15,60 @@ popd  > /dev/null
 PATH_SCRIPT=$PATH_TOOL/script
 
 FILE=$PATH_TOOL/"hw_data.sh"
-rm $FILE
-touch $FILE
+echo "#!/usr/bin/bash" > $FILE
 
+
+echo "############################### " >> $FILE
 echo "#########  GENERAL ############ " >> $FILE
+echo "############################### " >> $FILE
+
+echo "#-- CPU GENERATION --" >> $FILE
+echo "export PM_HW_CPU_GENERATION=\"$PM_HW_CPU_GENERATION\"" >> $FILE
+echo "" >> $FILE
 
 echo "#-- CPU NAME --" >> $FILE
 echo "export PM_HW_CPU_NAME=\"$PM_HW_CPU_NAME\"" >> $FILE
 echo "" >> $FILE
+
 echo "#-- Number of sockets on the server --" >> $FILE
 echo "export PM_HW_SOCKET=$PM_HW_SOCKET" >> $FILE
 echo "" >> $FILE
+
 echo "#-- Number of physical cores --" >> $FILE
-echo "export PM_HW_CORE_PHY=$PM_HW_CORE_PHY" >> $FILE
+echo "export PM_HW_CORE_PHY=$PM_HW_CPU_CORE_PHY" >> $FILE
 echo "" >> $FILE
+
 echo "#-- Number of threads --" >> $FILE
-echo "export PM_HW_THREADS=$PM_HW_THREADS" >> $FILE
+echo "export PM_HW_THREADS=$PM_HW_CPU_THREADS" >> $FILE
 echo "" >> $FILE
+
 echo "#-- Frequency base --" >> $FILE
 echo "export PM_HW_CPU_FREQ_BASE=$PM_HW_CPU_FREQ_BASE" >> $FILE
 echo "" >> $FILE
+
 echo "#-- Has Turbo ON ? --" >> $FILE
 echo "export PM_HW_CPU_FREQ_TURBO=$PM_HW_CPU_HAS_TURBO" >> $FILE
+echo "" >> $FILE
 
+
+
+echo "\n\n" >> $FILE
+echo "############################### " >> $FILE
+echo "#########  MEMORY  ############ " >> $FILE
+echo "############################### " >> $FILE
+
+
+echo "#-- CACHE L1 --" >> $FILE
+echo "" >> $FILE
+
+echo "#-- CACHE L2 --" >> $FILE
+echo "" >> $FILE
+
+echo "#-- CACHE L3 --" >> $FILE
+echo "" >> $FILE
+
+echo "#-- LAST LEVEL CACHE (L4, MCDRAM...) --" >> $FILE
+echo "" >> $FILE
 
 
 
