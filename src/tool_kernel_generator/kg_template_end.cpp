478f1d17e1520dc6386edfaf8a1c72b5460d34a1
Jean Pourroy <jean.pourroy@gmail.com>
2017-08-24 14:51:11 +0200

    working version

--- src/tool_kernel_generator/kg_template_end.cpp
+++ src/tool_kernel_generator/kg_template_end.cpp
@@ -1,16 +1,16 @@
-        //--------------
-//        cycleInEnd = rdtsc();
-//        timeEnd = mygettime();
-//        pairArr[i] = make_pair(cycleInEnd - cycleInStart, timeEnd - timeStart);
-    }
-    ofstream mFile_template_start (TMP_FILE_monitoring, std::ios_base::binary);
+//--------------
+cycleInEnd = rdtsc();
+timeEnd = mygettime();
+pairArr[i] = make_pair(cycleInEnd - cycleInStart, timeEnd - timeStart);
+}
+ofstream mFile_template_start (TMP_FILE_monitoring, std::ios_base::binary);
 
 
-    for (int i = 0; i < NB_lOOP; ++i) {
-        mFile_template_start << pairArr[i].first << " " << to_string(pairArr[i].second) << endl;
+for (int i = 0; i < NB_lOOP; ++i) {
+mFile_template_start << pairArr[i].first << " " << to_string(pairArr[i].second) << endl;
 //        cout                 << pairArr[i].first << " " << to_string(pairArr[i].second) << endl;
-    }
-    mFile_template_start.close();
+}
+mFile_template_start.close();
 
-    return 0;
+return 0;
 }
