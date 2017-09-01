d00e10d66f86848522d1a7c5b070e35c4a2ee9b9
Jean Pourroy <jean.pourroy@gmail.com>
2017-09-01 18:10:22 +0200

    mechanisme de verification flop

--- src/tool_kernel_generator/kg_template_start.cpp
+++ src/tool_kernel_generator/kg_template_start.cpp
@@ -7,38 +7,6 @@
 #include <utility>
 #include <fstream>
 
-/*
- *   unsigned long long int CUMUL_CYCLE = 0;
-        double CUMUL_TIME = 0;
-
-        __asm__ ("myBench: ");
-        cycleInStart = rdtsc();
-        timeStart = mygettime();
-
-        __asm__(
-                "vmulpd %xmm0, %xmm1, %xmm2; "
-                "vmulpd %xmm0, %xmm1, %xmm3; "
-                "vmulpd %xmm0, %xmm1, %xmm4; "
-                "vmulpd %xmm0, %xmm1, %xmm5; "
-                "vmulpd %xmm0, %xmm1, %xmm6; "
-                "vmulpd %xmm0, %xmm1, %xmm7; "
-                "vmulpd %xmm0, %xmm1, %xmm8; "
-                "vmulpd %xmm0, %xmm1, %xmm9; "
-                "vmulpd %xmm0, %xmm1, %xmm10; "
-                "vmulpd %xmm0, %xmm1, %xmm11; ");
-
-        cycleInEnd = rdtsc();
-        timeEnd = mygettime();
-
-        CUMUL_CYCLE += cycleInEnd - cycleInStart;
-        CUMUL_TIME  += timeEnd - timeStart;
-        __asm__(
-                "  sub    $0x1, %%eax;"
-                "  jnz    myBench" : : "a" (200000));//--------------
-        pairArr[i] = make_pair(CUMUL_CYCLE, CUMUL_TIME);
- */
-
-
 uint64_t rdtsc() {
     uint32_t lo, hi;
     __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
@@ -59,11 +27,12 @@ double mygettime() {
 }
 
 int main(int argc, char **argv) {
-    unsigned long long int nbCycleIn;
-    unsigned long long int cycleInStart, cycleInEnd;
-    double timeStart, timeEnd, timeSpent;
+    unsigned long long  nbCycleIn;
+    unsigned long long  cycleInStart, cycleInEnd;
+    unsigned long long  cycle_total, instructions_total, instructions_executed, instructions_executed_total = 0;
+    double IPC = 0.0;
     int i;
-    double ipc;
+    double timeStart, timeEnd, timeSpent;
     std::pair<int, double> *pairArr = new pair<int, double>[1000000];
 
     for (i = 0; i < NB_lOOP; i++) {
