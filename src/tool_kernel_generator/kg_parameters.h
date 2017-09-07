a49d6081c4b5983c12bb25970f7e266749364135
Jean Pourroy <jean.pourroy@gmail.com>
2017-09-07 16:32:42 +0200

    consolidation kg, benchmark multiply

--- src/tool_kernel_generator/kg_parameters.h
+++ src/tool_kernel_generator/kg_parameters.h
@@ -18,17 +18,18 @@ static std::map<int, std::string> mapParameter = {{SCALAR, "SCALAR"},
 
 class KG_parameters {
 public:
-    int P_WIDTH;
+    int         P_WIDTH;
     std::string P_OPERATIONS;
-    int P_BIND;
-    bool P_DEPENDENCY;
+    int         P_BIND;
+    bool        P_DEPENDENCY;
     std::string P_PRECISION;
-    bool P_VERBOSE;
-    bool P_HELP;
-    unsigned P_LOOP_SIZE;
-    bool P_GRAPH;
-    unsigned P_SAMPLES;
-    bool P_DEBUG;
+    bool        P_VERBOSE;
+    bool        P_HELP;
+    unsigned    P_LOOP_SIZE;
+    bool        P_GRAPH;
+    unsigned    P_SAMPLES;
+    bool        P_COUNT;
+    bool        P_DEBUG;
 
     KG_parameters();
 
--- src/tool_kernel_generator/kg_template_end.cpp
+++ src/tool_kernel_generator/kg_template_end.cpp
@@ -4,22 +4,26 @@
         pairArr[i] = make_pair(cycleInEnd - cycleInStart, timeEnd - timeStart);
         cycle_total += cycleInEnd - cycleInStart;
         instructions_executed_total += instructions_executed;
-        }
+    }
     ofstream mFile_template_start(TMP_FILE_monitoring, std::ios_base::binary);
 
     instructions_total = (NB_lOOP_IN * NB_lOOP);
     instructions_total *= NB_INST;
-    IPC = double(instructions_total) / double (cycle_total);
-    instructions_executed_total -= (NB_INST * NB_lOOP);
+    IPC = double(instructions_total) / double(cycle_total);
 
-    cout << setw (25) <<  "instructions_total" << setw (25) << "instructions_executed"  << setw (20) << "cycle_total" << setw (10) <<  "IPC"<<  std::endl;
-    cout << setw (25) <<  instructions_total   << setw (25) << instructions_executed_total    << setw (20) << cycle_total   << setw (10) << IPC <<  std::endl;
+    if (P_COUNT) {
+        instructions_executed_total -= (NB_INST * NB_lOOP);
+        cout << setw(25) << "instructions_total" << setw(25) << "instructions_executed"     << setw(20)   << "cycle_total" << setw(10) << "IPC" << std::endl;
+        cout << setw(25) << instructions_total   << setw(25) << instructions_executed_total << setw(20)   << cycle_total   << setw(10) <<  IPC  << std::endl;
 
+    } else {
+        cout << setw(25) << "instructions_total" << setw(20) << "cycle_total" << setw(10) << "IPC" << std::endl;
+        cout << setw(25) << instructions_total   << setw(20) << cycle_total   << setw(10) << IPC   << std::endl;
 
+    }
 
-        for (int i = 0; i < NB_lOOP; ++i) {
+    for (int i = 0; i < NB_lOOP; ++i) {
         mFile_template_start << pairArr[i].first << " " << to_string(pairArr[i].second) << endl;
-//        cout                 << pairArr[i].first << " " << to_string(pairArr[i].second) << endl;
     }
     mFile_template_start.close();
 
