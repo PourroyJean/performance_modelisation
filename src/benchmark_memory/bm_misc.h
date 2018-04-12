86b3eff617062901b58bd5da5bc24500d8b04c44
Jean Pourroy <jean.pourroy@gmail.com>
2018-04-12 18:59:32 +0200

    memory benchmark release 0.1

---
+++ src/benchmark_memory/bm_misc.h
@@ -0,0 +1,16 @@
+//
+// Created by Jean Pourroy on 11/04/2018.
+//
+
+#ifndef __bm_common_H__
+#define __bm_common_H__
+
+
+#define BM_DATA_TYPE double
+#define LOG10 2.30258509299404568401
+#define BIG_VAL 999999999.0
+#define MIN_OPS_PER_SCAN 128     	/* we do not measure if less MIN_OPS_PER SCAN operations per loop */
+
+
+
+#endif //__bm_common_H__
