3a39991c896f4fef3ad8ae608b7f96210143a630
Jean Pourroy <jean.pourroy@gmail.com>
2018-04-17 14:17:46 +0200

    dmlmem release 1.0: working well

--- src/benchmark_memory/bm_misc.h
+++ src/benchmark_memory/bm_misc.h
@@ -5,11 +5,14 @@
 #ifndef __bm_common_H__
 #define __bm_common_H__
 
+#include <limits>
 
 #define BM_DATA_TYPE double
 #define LOG10 2.30258509299404568401
-#define BIG_VAL 999999999.0
+#define BIG_VAL std::numeric_limits<BM_DATA_TYPE>::max()
+//#define BIG_VAL 999999999.0
 #define MIN_OPS_PER_SCAN 128     	/* we do not measure if less MIN_OPS_PER SCAN operations per loop */
+
 
 
 //TODO
