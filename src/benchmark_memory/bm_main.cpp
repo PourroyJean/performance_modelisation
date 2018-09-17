3c46aca12952efc2fa519da5ba7d506925fcc4e3
Jean Pourroy <jean.pourroy@gmail.com>
2018-09-17 16:28:43 +0200

    MPI compilation

--- src/benchmark_memory/bm_main.cpp
+++ src/benchmark_memory/bm_main.cpp
@@ -10,7 +10,8 @@
 #include <misc.h>
 #include "bm_misc.h"
 #include <sys/shm.h>        // Large parge
-#include <mpi.h>
+#include <unistd.h>
+
 
 
 #include <sys/shm.h>
@@ -36,59 +37,81 @@ double get_micros();
 double total_loops = 0.01; //TODO metre a 0
 
 BM_DATA_TYPE *mat;  //THE matrix :)
+bool WITH_MPI = false;
+
 #define ADDR (0x0UL)
 
 
+
+
+
+
 int shmid = -2;
+int mpi_rank = 0;
+int mpi_size = 1;
+
+std::stringstream black_hole;
 
 
-#ifdef  WITH_MPI
-#define MESS  "WITH MPI  "
+#ifdef  COMPILED_WITH_MPI
+#include <mpi.h>
 #else
 #define MESS "NO MPI"
 #endif
 
 
+
+
+
 int main(int argc, const char *argv[]) {
+    fclose (stderr);
     double tot, band;
     double start_time, end_time = 0.0;
-    int rank, size;
 
     char **m_argv = (char **) argv;
 //    strdup(*argv);
-#ifdef  WITH_MPI
-    MPI_Init(&argc, &m_argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+#ifdef  COMPILED_WITH_MPI
+    if ( ! is_Launched_with_mpirun()){
+        cout << "This program was compiled with MPI, you should use mpirun to launch it\n";
+        return 1;
+    }
 #endif
 
-    cout << "MPI : " << MESS << endl;
-    printf("Hello world from processor rank %d out of %d processors\n", rank, size);
+    //MPI INIT
+#ifdef  COMPILED_WITH_MPI
+    MPI_Init(&argc, &m_argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+    DEBUG << "Hello world from processor "  << mpi_rank << "out of " << mpi_size << "processors\n";
+#endif
+
+    bm_parameters *my_parameters;
 
     //Parse argument, initiliaze the argument structure and print the configuration
-    cout << "\n ------------- CONFIGURATION --------------\n";
-    bm_parameters *my_parameters = new bm_parameters();
+    COUT_MPI << "\n ------------- CONFIGURATION --------------\n";
+    my_parameters = new bm_parameters();
     my_parameters->init_arguments(argc, argv);
-    cout << "\n-- The following configuration will be used for the benchmark:\n";
-    my_parameters->print_configuration();
-
+    if (mpi_rank == 0) {
+        cout << "\n-- The following configuration will be used for the benchmark:\n";
+        my_parameters->print_configuration();
+    }
     //Matrice initialisation
     init_mat(my_parameters);
 
+    COUT_MPI << "\n ---------------- BENCHMARK ----------------\n";
 
-    cout << "\n ---------------- BENCHMARK ----------------\n";
-
-
+    MPI_BARRIER
     start_time = get_micros();
     work(my_parameters);
     end_time = get_micros();
+    MPI_BARRIER
 
+    if (mpi_rank == 0) {
+        cout << "\n ---------------- RESULTS ----------------\n";
+        tot = (end_time - start_time) * 1000.0 / total_loops;
+        band = my_parameters->m_CACHE_LINE / tot;
 
-    cout << "\n ---------------- RESULTS ----------------\n";
-    tot = (end_time - start_time) * 1000.0 / total_loops;
-    band = my_parameters->m_CACHE_LINE / tot;
-
-    if (rank == 0) {
         printf("%20s    %-10s \n", "Name", my_parameters->m_prefix.c_str());
         printf("%20s    %-10f \n", "Total micros", end_time - start_time);
         printf("%20s    %-10f \n", "Total Loops", total_loops);
@@ -103,12 +126,9 @@ int main(int argc, const char *argv[]) {
         free(mat);
     }
 
-    #ifdef  WITH_MPI
-    MPI_Finalize();
-    #endif
+    MPI_FINALIZE
 
-//    printf("_ %s  total micros = %1.0f total loops = %1.0f perfo = %1.2f ns/loop band= %1.3lf GB/s\n",
-//           my_parameters->m_prefix.c_str(), end_time - start_time, total_loops, tot, band);
+
     return (0);
 }
 
@@ -120,26 +140,27 @@ int work(bm_parameters *p) {
 
 
     //Print header
-    printf("_ %s Stride  S   ->", p->m_prefix.c_str());
-    for (step = p->m_MIN_STRIDE; step <= p->m_MAX_STRIDE; step *= 2) {
+    if (mpi_rank == 0) {
+        printf("_ %s Stride  S   ->", p->m_prefix.c_str());
+        for (step = p->m_MIN_STRIDE; step <= p->m_MAX_STRIDE; step *= 2) {
 
-        if (p->m_DISP == DISP_MODE::AVERAGE) printf("%11d", step * 8);
-        if (p->m_DISP == DISP_MODE::BEST) printf("%11d", step * 8);
-        if (p->m_DISP == DISP_MODE::TWO) printf("%11d%11d", step * 8, step * 8);
-        if (p->m_DISP == DISP_MODE::ALL) printf("%11d%11d%11d", step * 8, step * 8, step * 8);
+            if (p->m_DISP == DISP_MODE::AVERAGE) printf("%11d", step * 8);
+            if (p->m_DISP == DISP_MODE::BEST) printf("%11d", step * 8);
+            if (p->m_DISP == DISP_MODE::TWO) printf("%11d%11d", step * 8, step * 8);
+            if (p->m_DISP == DISP_MODE::ALL) printf("%11d%11d%11d", step * 8, step * 8, step * 8);
+        }
+        printf("\n");
+
+        printf("_ %s Value       ->", p->m_prefix.c_str());
+        for (step = p->m_MIN_STRIDE; step <= p->m_MAX_STRIDE; step *= 2) {
+
+            if (p->m_DISP == DISP_MODE::AVERAGE) printf("%11s", "AVERAGE");
+            if (p->m_DISP == DISP_MODE::BEST) printf("%11s", "BEST");
+            if (p->m_DISP == DISP_MODE::TWO) printf("%11s%11s", "BEST", "AVERAGE");
+            if (p->m_DISP == DISP_MODE::ALL) printf("%11s%11s%11s", "BEST", "WORST", "AVERAGE");
+        }
+        printf("\n");
     }
-    printf("\n");
-
-    printf("_ %s Value       ->", p->m_prefix.c_str());
-    for (step = p->m_MIN_STRIDE; step <= p->m_MAX_STRIDE; step *= 2) {
-
-        if (p->m_DISP == DISP_MODE::AVERAGE) printf("%11s", "AVERAGE");
-        if (p->m_DISP == DISP_MODE::BEST) printf("%11s", "BEST");
-        if (p->m_DISP == DISP_MODE::TWO) printf("%11s%11s", "BEST", "AVERAGE");
-        if (p->m_DISP == DISP_MODE::ALL) printf("%11s%11s%11s", "BEST", "WORST", "AVERAGE");
-    }
-    printf("\n");
-
 
     //Work
     for (log_max_index = p->m_MIN_LOG10; log_max_index < p->m_MAX_LOG10 + .0000001; log_max_index += p->m_STEP_LOG10) {
@@ -150,20 +171,21 @@ int work(bm_parameters *p) {
             break;
         }
 
-        printf("_ %s K = %10s", p->m_prefix.c_str(), convert_size(istride * 1024).c_str());
-
-
-        string ping = "K = " + convert_size(istride * 1024);
-        yamb_annotate_set_event(ping.c_str(), "blue");
-
+        if (mpi_rank == 0) {
+            printf("_ %s K = %10s", p->m_prefix.c_str(), convert_size(istride * 1024).c_str());
+            string ping = "K = " + convert_size(istride * 1024);
+            yamb_annotate_set_event(ping.c_str(), "blue");
+        }
 
         for (step = p->m_MIN_STRIDE; step <= p->m_MAX_STRIDE; step *= 2) {
             double gb;
             best_measure = BIG_VAL;
             worst_measure = 0;
             sum_measures = 0.0;
-            ping = "Stride : " + to_string(step * 8);
-            yamb_annotate_set_event(ping.c_str(), "red");
+            if (mpi_rank == 0) {
+                string ping = "Stride : " + to_string(step * 8);
+                yamb_annotate_set_event(ping.c_str(), "red");
+            }
             for (measure = 0; measure < p->m_MAX_MEASURES; measure++) {
                 double t;
 
@@ -184,15 +206,11 @@ int work(bm_parameters *p) {
                     if (repeat == 0)repeat = 1;
 
                     // --- BENCHMARK MEASURE ---
-                    #ifdef  WITH_MPI
-                    MPI_Barrier(MPI_COMM_WORLD);
-                    #endif
+                    MPI_BARRIER
                     time1 = get_micros();
                     num_ops = p->m_BENCHMARK(p, max_index, step, repeat, ops_per_scan);
                     time2 = get_micros();
-                    #ifdef  WITH_MPI
-                    MPI_Barrier(MPI_COMM_WORLD);
-                    #endif
+                    MPI_BARRIER
 
                     t = (time2 - time1) * 1000.0;
                     if (num_ops != 0)
@@ -204,6 +222,9 @@ int work(bm_parameters *p) {
                     sum_measures += t;
                 }
             }
+
+            if (mpi_rank != 0)
+                continue;
 
             if (num_ops < BIG_VAL) {
 
@@ -244,11 +265,9 @@ int work(bm_parameters *p) {
             } else {
                 printf("%11s", "-");
             }
-
         }
 
-        printf("\n");
-        fflush(stdout);
+        COUT_MPI << endl << flush;
 //        exit(0);
     }
 }
--- src/benchmark_memory/bm_misc.h
+++ src/benchmark_memory/bm_misc.h
@@ -6,6 +6,8 @@
 #define __bm_common_H__
 
 #include <limits>
+#include <sstream>      // std::stringstream
+
 
 #define BM_DATA_TYPE double
 #define LOG10 2.30258509299404568401
@@ -14,11 +16,34 @@
 #define MIN_OPS_PER_SCAN 128     	/* we do not measure if less MIN_OPS_PER SCAN operations per loop */
 
 
+// --------- MPI MACRO ------------
+#ifdef COMPILED_WITH_MPI
+#define MPI_BARRIER MPI_Barrier(MPI_COMM_WORLD);
+#else
+#define MPI_BARRIER
+#endif
+
+#ifdef COMPILED_WITH_MPI
+#define MPI_FINALIZE MPI_Finalize();
+#else
+#define MPI_FINALIZE
+#endif
+// --------------------------------
+
 
 //TODO
 extern BM_DATA_TYPE *mat;  //THE matrix :)
 
+extern int mpi_rank;
+extern int mpi_size;
+extern std::stringstream black_hole;
+#define COUT_MPI ((mpi_rank == 0) ? std::cout : black_hole)
+#define DEBUG_MPI ((mpi_rank == 0) ? DEBUG : black_hole)
 
+
+extern bool WITH_MPI;
+
+bool is_Launched_with_mpirun ();
 
 
 #endif //__bm_common_H__
