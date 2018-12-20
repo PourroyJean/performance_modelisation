71748a1250e78f0348b59a485401d3995405a7bd
Jean Pourroy <jean.pourroy@gmail.com>
2018-12-20 12:05:54 +0100

    New architecture seems ok. - Static FILE for both lines - Oprofile class implemented

--- src/tool_oprofile/merger/op_misc.cpp
+++ src/tool_oprofile/merger/op_misc.cpp
@@ -33,6 +33,20 @@ ui64 stoullhexa(string str) {
     return (add);
 }
 
+const vector<string> my_split(const string &s, const char &c) {
+    string buff{""};
+    vector<string> v;
+
+    for (auto n:s) {
+        if (n != c) buff += n;
+        else if (n == c && buff != "") {
+            v.push_back(buff);
+            buff = "";
+        }
+    }
+    if (buff != "") v.push_back(buff);
+    return v;
+}
 
 const vector<string> split(const string &s, const char &c) {
     string buff{""};
