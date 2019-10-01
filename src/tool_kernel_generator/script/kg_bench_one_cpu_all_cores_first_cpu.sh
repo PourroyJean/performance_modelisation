c50ed7c189c9d03bc6d8a7187dcdf22830a6a93d
Jean Pourroy <jean.pourroy@gmail.com>
2019-10-01 14:09:01 +0200

    dependency option

--- src/tool_kernel_generator/script/kg_bench_one_cpu_all_cores_first_cpu.sh
+++ src/tool_kernel_generator/script/kg_bench_one_cpu_all_cores_first_cpu.sh
@@ -8,14 +8,19 @@ BENCH="assembly"
 NB_CORES_TO_BENCH=`lscpu | grep "Core(s)" | awk '{print $4}'` #TOTAL
 
 
+
 #  -- FIND THE FIRST CORE OF THE SECOND SOCKET --
 #  numactl --hardware
 #  node 0 cpus: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53
 #  node 1 cpus: 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71
+
+# INTEL A REMETTRE TODO
 NEXT_SOCKET=`numactl --hardware | grep "node 1" | head -n 1 | awk '{print $4}'`
-
-
 CORE_HARDWARE=`numactl --hardware | grep "node 0 cpus" | awk '{for(i=4;i<=NF;++i)printf $i " "}'`
+
+
+# AMD A DELETE TODO
+#CORE_HARDWARE="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63"
 
 
 arr=($CORE_HARDWARE)
@@ -34,10 +39,14 @@ echo  "Launching $BENCH on {${CORE_LIST[*]}}"
 
 for core in "${CORE_LIST[@]}" ; do
      if [[ "$core" == ${CORE_LIST[-1]} ]]; then
-        ./$BENCH $core
-        continue
+        PERF_1=`./$BENCH $core  | grep Double | awk '{print $3}'`
+#        echo "PERF 1 -- $PERF_1"
+        PERF_1_DEC=$(printf "%.0f\n" $PERF_1)
+#        echo $PERF_1_DEC
+        echo $((PERF_1_DEC / 1000000000 * NB_CORES_TO_BENCH))
+    else
+        ./$BENCH $core  | grep "Running" &
     fi
-     ./$BENCH $core  | grep "Running" &
 
 done
 
