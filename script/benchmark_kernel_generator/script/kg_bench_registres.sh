78d90c52ff179224422582bf27832832f6d6aa5c
Jean Pourroy <jean.pourroy@gmail.com>
2019-10-01 14:19:41 +0200

    rename tool_kernel_generator into benchmark_kernel_generator

--- script/benchmark_kernel_generator/script/kg_bench_registres.sh
+++ script/benchmark_kernel_generator/script/kg_bench_registres.sh
@@ -11,10 +11,10 @@ for ((i=1; i<=$MAX; i++)); do
     printf "%50s " ${#operation}
 
 
-#    ./bin/tool_kernel_generator/kg -L 10000  -W 128 -O $operation -D true -U 10 | grep  "^IPC" | awk '{print $2}'
+#    ./bin/benchmark_kernel_generator/kg -L 10000  -W 128 -O $operation -D true -U 10 | grep  "^IPC" | awk '{print $2}'
     ./bin/tool_kernel_generator/kg -W 512  -P double   -S 10 -L 100000 -U 10  -O $operation  -D 0 | grep "_value_" | awk '{print $6}'
 
-#    ./bin/tool_kernel_generator/kg  -W 512  -P double   -S 10 -L 9000 -U 10  -O mmmmmmmm  | grep  "^IPC" | awk '{print $2}'
+#    ./bin/benchmark_kernel_generator/kg  -W 512  -P double   -S 10 -L 9000 -U 10  -O mmmmmmmm  | grep  "^IPC" | awk '{print $2}'
 done
 
 
