8042385ff6a771f06a18b86a80e2174b0cb74572
Jean Pourroy <jean.pourroy@gmail.com>
2019-10-01 12:48:16 +0200

    benchmark center scripts

---
+++ script/Grenoble_benchmarking_center/GRE.oprofile.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+TOP=$(pwd)
+BIN=$TOP/$1
+
+SCRIPT_DIR="`dirname \"$0\"`"
+SCRIPT_DIR="`( cd \"$SCRIPT_DIR\" && pwd )`"
+
+echo "$SCRIPT_DIR"
+
+
+# COMMAND
+#./GRE.oprofile.sh
+#grep -v "^ " op2 | more
+#objdump -d  bsmbench_compute_1cpu  > OBJ.18
+#./dmlv3 OBJ.18 op2 > op4.suivant
+
+
+
+
+$SCRIPT_DIR/GRE.jini.sh
+
+echo "Lancement en background" ; $BIN &
+
+$SCRIPT_DIR/GRE.jop.sh
+
+#./GRE.obj.sh $BIN
