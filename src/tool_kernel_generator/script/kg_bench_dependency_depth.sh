b82d4b5296161e15e470ca1d9d7e825bcb3805b0
Jean Pourroy <jean.pourroy@gmail.com>
2019-10-01 14:09:01 +0200

    dependency option

--- src/tool_kernel_generator/script/kg_bench_dependency_depth.sh
+++ src/tool_kernel_generator/script/kg_bench_dependency_depth.sh
@@ -2,8 +2,9 @@
 #export PATH=`dirname $0`:$PATH
 
 
+MIN=1
 MAX=30
-for ((i=0; i<=$MAX; i++)); do
+for ((i=0$MIN; i<=$MAX; i++)); do
     printf "./kg -D $i "
     ./bin/tool_kernel_generator/kg -W 512  -P double   -S 1 -L 9000000 -U 10  -O ffffffffffffffffff  -D $i | grep "_value_" | awk '{print $6}'
 done
