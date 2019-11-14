87d34964acb03ff8b3750dbf034174ed3c709a06
Jean Pourroy <jean.pourroy@gmail.com>
2019-11-14 15:20:32 +0100

    Argument parsing and display sum option

--- src/tool_oprofile/merger/op_misc.cpp
+++ src/tool_oprofile/merger/op_misc.cpp
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include "op_misc.h"
 #include <fstream>
-
+#include "../../common/AnyOption/anyoption.h"
 
 ui64 stoullhexa(string str) {
     ui64 add = 0;
@@ -46,4 +46,85 @@ const vector<string> my_split(const string &s, const char &c) {
     return v;
 }
 
+
+void parse_argument(int argc, char *argv[], AnyOption *opt) {
+
+    /* 1. CREATE AN OBJECT */
+    /* 2. SET PREFERENCES  */
+    // opt->noPOSIX(); /* do not check for POSIX style character options */
+    // opt->setVerbose(); /* print warnings about unknown options */
+    opt->autoUsagePrint(true); /* print usage for bad options */
+
+    /* 3. SET THE USAGE/HELP   */
+    opt->addUsage("usage: ./merger --object <path> --profile <path> [--sum]");
+    opt->addUsage("   -h  --help                Prints this help ");
+    opt->addUsage("   -o  --object  <path>      Objdump  output file");
+    opt->addUsage("   -p  --profile <path>      Oprofile output file");
+    opt->addUsage("   -sq  --sum                 Display sum section");
+
+    /* 4. SET THE OPTION STRINGS/CHARACTERS */
+
+    /* by default all  options  will be checked on the command line and from
+     * option/resource file */
+    opt->setFlag(
+            "help",
+            'h'); /* a flag (takes no argument), supporting long and short form */
+    opt->setFlag(
+            "sum",
+            's'); /* a flag (takes no argument), supporting long and short form */
+    opt->setOption(
+            "object",
+            'o'); /* an option (takes an argument), supporting long and short form */
+    opt->setOption(
+            "profile",
+            'p'); /* an option (takes an argument), supporting long and short form */
+
+
+    /* 5. PROCESS THE COMMANDLINE AND RESOURCE FILE */
+
+    /* go through the command line and get the options  */
+    opt->processCommandArgs(argc, argv);
+
+    if (!opt->hasOptions()) { /* print usage if no options */
+        opt->printUsage();
+        delete opt;
+        return;
+    }
+
+    /* 6. GET THE VALUES */
+
+    cout << " --------------- CONFIGURATION -------------\n";
+    if (opt->getFlag("help") || opt->getFlag('h')) {
+        opt->printUsage();
+        exit(0);
+    }
+
+
+    if (opt->getValue('o') != NULL || opt->getValue("object") != NULL)
+        cout << "object        " << opt->getValue('o') << endl;
+    if (opt->getValue('p') != NULL || opt->getValue("profile") != NULL)
+        cout << "profile       " << opt->getValue('p') << endl;
+
+    if (opt->getFlag("sum") || opt->getFlag('s')) {
+        cout << "Display sum   true\n";
+    } else {
+        cout << "Display sum   false\n";
+    }
+
+    /* 7. GET THE ACTUAL ARGUMENTS AFTER THE OPTIONS */
+    for (int i = 0; i < opt->getArgc(); i++) {
+        cout << "arg = " << opt->getArgv(i) << endl;
+    }
+
+    if ((opt->getValue('o') == NULL && opt->getValue("object") == NULL) ||
+        (opt->getValue('p') == NULL && opt->getValue("profile") == NULL)) {
+        cerr << "Need at least 2 arguments\n";
+        opt->printUsage();
+        exit(-1);
+    }
+
+    cout << " -------------------------------------------\n";
+
+    /* 8. DONE */
+}
 
