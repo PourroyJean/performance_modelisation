c14da217e3af865b8d7ee74a0b4542dc913afd77
Jean Pourroy <jean.pourroy@gmail.com>
2020-08-10 14:30:51 +0200

    minor fix

--- src/benchmark_dml_mem/dml_parameters.cpp
+++ src/benchmark_dml_mem/dml_parameters.cpp
@@ -303,7 +303,7 @@ int Dml_parameters::setup_parser(int argc, const char *argv[]) {
             0, // Required?
             1, // Number of args expected.
             0, // Delimiter if expecting multiple args.
-            "TODO", // Help description.
+            "CPU frequency", // Help description.
             "--ghz" // Flag token.
     );
 
